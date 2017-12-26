21a98fa8aaa2bf5a855fb392428688accd0ad562
bondarsasha <bondaralexandr1991@gmail.com>
2017-12-26 06:35:23 +0200

    firstChange

---
+++ js/user.js
@@ -0,0 +1,12 @@
+document.addEventListener("DOMContentLoaded", ready);
+function ready() {
+
+    var start = document.getElementById('startGame');
+
+    start.addEventListener("click", function(go) {
+        console.log(go);
+    })
+
+
+
+}
